7bb961fad41685a1cf2891b777772a6db1b733c9
Tri Le <trile@users.noreply.github.com>
2017-08-16 19:11:54 +0700

    Remove some debug messages in svbtle.js

--- themes/svbtle/source/js/svbtle.js
+++ themes/svbtle/source/js/svbtle.js
@@ -1,8 +1,6 @@
 // document.ready(function(){
   document.querySelector('#hamburger').addEventListener('click', function(){
-    console.log("hello");
     var nav = document.querySelector('#main-nav');
-    console.log(nav);
     nav.classList.toggle('open');
   });
 // });
